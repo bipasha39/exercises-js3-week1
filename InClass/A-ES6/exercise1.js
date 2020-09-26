d807237c5c9f27e860711e18c9da8a5a000db734
bipasha39 <physiobipasha39@gmail.com>
2020-09-26 12:24:07 +0200

    inclass

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -2,24 +2,28 @@
 function arrow() {
   return "es6 is awesome!";
 }
+console.log(arrow);
 
 //Use the es6 syntax for default parameter
 function defaultParameter(name) {
   var name = name || "sam";
   return name;
 }
+console.log(defaultParameter);
 
 // Use the spread operator to combine arr1 and arr2
 function combineArrays(arr1, arr2) {
   return arr1.concat(arr2);
 }
-
+console.log(combineArrays);
 //use destructuring to return the object's cyf property
 function destructuring(obj) {
   return obj.cyf;
 }
+console.log(destructuring);
 
 // use template literal to return a string with the sum of a and b
 function templateString(a, b) {
   return "The sum is equal to " + (a + b).toString();
 }
+console.log (templateString);
